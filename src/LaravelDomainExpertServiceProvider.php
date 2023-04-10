08402953ebb821bc7b4e904da5e94c91f949c203
salehhashemi1992 <81674631+salehhashemi1992@users.noreply.github.com>
2023-04-10 15:07:10 +0430

    service provider updates

--- src/LaravelDomainExpertServiceProvider.php
+++ src/LaravelDomainExpertServiceProvider.php
@@ -3,6 +3,7 @@
 namespace Salehhashemi\LaravelDomainExpert;
 
 use Illuminate\Support\ServiceProvider;
+use Salehhashemi\LaravelDomainExpert\Console\DomainMakeCommand;
 
 class LaravelDomainExpertServiceProvider extends ServiceProvider
 {
@@ -11,50 +12,15 @@ class LaravelDomainExpertServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        /*
-         * Optional methods to load your package assets
-         */
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'laravel-domain-expert');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'laravel-domain-expert');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__.'/routes.php');
-
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../config/config.php' => config_path('laravel-domain-expert.php'),
+                __DIR__.'/../config/domain-expert.php' => config_path('domain-expert.php'),
             ], 'config');
 
-            // Publishing the views.
-            /*$this->publishes([
-                __DIR__.'/../resources/views' => resource_path('views/vendor/laravel-domain-expert'),
-            ], 'views');*/
-
-            // Publishing assets.
-            /*$this->publishes([
-                __DIR__.'/../resources/assets' => public_path('vendor/laravel-domain-expert'),
-            ], 'assets');*/
-
-            // Publishing the translation files.
-            /*$this->publishes([
-                __DIR__.'/../resources/lang' => resource_path('lang/vendor/laravel-domain-expert'),
-            ], 'lang');*/
-
             // Registering package commands.
-            // $this->commands([]);
+            $this->commands([
+                DomainMakeCommand::class,
+            ]);
         }
-    }
-
-    /**
-     * Register the application services.
-     */
-    public function register()
-    {
-        // Automatically apply the package configuration
-        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'laravel-domain-expert');
-
-        // Register the main class to use with the facade
-        $this->app->singleton('laravel-domain-expert', function () {
-            return new LaravelDomainExpert;
-        });
     }
 }
