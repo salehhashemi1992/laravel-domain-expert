f072e942521cbeb1a3f1e5e96d171a9d23dfb203
salehhashemi1992 <81674631+salehhashemi1992@users.noreply.github.com>
2023-04-10 14:59:02 +0430

    domain make command implementation

---
+++ src/Console/DomainMakeCommand.php
@@ -0,0 +1,174 @@
+<?php
+
+namespace Salehhashemi\LaravelDomainExpert\Console;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Str;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+
+class DomainMakeCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $name = 'make:domain';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new domain directory structure.';
+
+    /**
+     * The type of files to generate.
+     *
+     * @var string
+     */
+    protected string $type = 'Route';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle(): int
+    {
+        $domain = $this->argument('domain');
+        $domainsDirectory = $this->getDomainsDirectory();
+
+        if (!$this->createNewDomainDirectory($domain, $domainsDirectory)) {
+            $this->error("{$domain} domain already exists! Will not overwrite existing domain.");
+            return 1;
+        }
+
+        $this->info("Domain created successfully in {$domainsDirectory}");
+
+        return 0;
+    }
+
+    /**
+     * Get the application Domains directory path.
+     *
+     * @return string
+     */
+    private function getDomainsDirectory(): string
+    {
+        $domainDirectory = app_path('Domains');
+
+        if (!file_exists($domainDirectory)) {
+            mkdir($domainDirectory);
+        }
+
+        return $domainDirectory;
+    }
+
+    /**
+     * Create a new domain directory with the necessary folders.
+     *
+     * @param string $domain
+     * @param string $domainsDirectory
+     * @return bool
+     */
+    private function createNewDomainDirectory(string $domain, string $domainsDirectory): bool
+    {
+        $newDomainDirectory = "{$domainsDirectory}/{$domain}";
+
+        if (file_exists($newDomainDirectory)) {
+            return false;
+        }
+
+        mkdir($newDomainDirectory);
+
+        $folders = [
+            'Exceptions',
+            'Http/Controllers',
+            'Http/Middleware',
+            'Http/Requests',
+            'Jobs',
+            'Models',
+            'Repositories',
+            'Services',
+            'resources/css',
+            'resources/js',
+            'resources/views',
+            'routes'
+        ];
+
+        foreach ($folders as $folder) {
+            mkdir("{$newDomainDirectory}/{$folder}", 0777, true);
+        }
+
+        $this->createController();
+
+        return true;
+    }
+
+    /**
+     * Create a controller for the new domain.
+     *
+     * @return void
+     */
+    protected function createController(): void
+    {
+        $domain = Str::studly(class_basename($this->argument('domain')));
+
+/*        $this->call(
+            'domain:make:controller',
+            $this->getControllerOptions($domain)
+        );
+
+        $this->call(
+            'domain:make:routes',
+            $this->getRoutesOptions($domain)
+        );*/
+    }
+
+    /**
+     * Get the options for creating a controller.
+     *
+     * @param string $domain
+     * @return array
+     */
+    protected function getControllerOptions(string $domain): array
+    {
+        return [
+            'domain' => $domain,
+            'name' => "{$domain}Controller",
+        ];
+    }
+
+    /**
+     * Get the options for creating routes.
+     *
+     * @param string $domain
+     * @return array
+     */
+    protected function getRoutesOptions(string $domain): array
+    {
+        return [
+            'domain' => $domain,
+            'name' => $domain,
+            '--controller' => "{$domain}Controller"
+        ];
+    }
+
+    /**
+     * Get the console command arguments.
+     *
+     * @return array
+     */
+    protected function getArguments(): array
+    {
+        return [
+            [
+                'domain',
+                InputArgument::REQUIRED,
+                'Name of the domain.',
+            ],
+        ];
+    }
+}
