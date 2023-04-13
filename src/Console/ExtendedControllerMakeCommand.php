4af2ba916efa356f808534cc56c13a150cbba5bb
salehhashemi1992 <81674631+salehhashemi1992@users.noreply.github.com>
2023-04-13 19:01:55 +0430

    implement a trait for the shared parts of extended commands

--- src/Console/ExtendedControllerMakeCommand.php
+++ src/Console/ExtendedControllerMakeCommand.php
@@ -3,38 +3,21 @@
 namespace Salehhashemi\LaravelDomainExpert\Console;
 
 use Illuminate\Routing\Console\ControllerMakeCommand;
-use Symfony\Component\Console\Input\InputOption;
 
 class ExtendedControllerMakeCommand extends ControllerMakeCommand
 {
+    use HandlesDomainOption;
+
+    protected string $folder = 'Controllers';
+
     /**
      * {@inheritdoc}
      */
     public function handle(): bool|null
     {
-        if ($this->option('domain')) {
-            $domains = $this->getDomains();
-
-            if (count($domains) === 0) {
-                $this->error('No domain folders are available in app/Domains.');
-
-                return false;
-            }
-
-            $domain = $this->choice('Which domain would you like to create the controller in?', $domains);
-            $this->input->setArgument('name', "App\\Domains\\{$domain}\\Http\\Controllers\\".$this->argument('name'));
-        }
+        $this->handleDomainOption();
 
         return parent::handle();
-    }
-
-    private function getDomains(): array
-    {
-        // Retrieve the list of domains from the Domains folder
-        $domainsDir = app_path('Domains');
-        $domains = array_diff(scandir($domainsDir), ['.', '..']);
-
-        return array_values($domains);
     }
 
     /**
@@ -43,7 +26,7 @@ class ExtendedControllerMakeCommand extends ControllerMakeCommand
     protected function getOptions(): array
     {
         $options = parent::getOptions();
-        $options[] = ['domain', 'd', InputOption::VALUE_NONE, 'Prompt for the domain to create the controller in'];
+        $options[] = $this->getDomainOption();
 
         return $options;
     }
---
+++ src/Console/HandlesDomainOption.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Salehhashemi\LaravelDomainExpert\Console;
+
+use Symfony\Component\Console\Input\InputOption;
+
+trait HandlesDomainOption
+{
+    protected function handleDomainOption(): bool|string
+    {
+        if ($this->option('domain')) {
+            $domains = $this->getDomains();
+
+            if (count($domains) === 0) {
+                $this->error('No domain folders are available in app/Domains.');
+
+                return false;
+            }
+
+            $domain = $this->choice('Which domain would you like to create the class in?', $domains);
+            $this->input->setArgument('name', "App\\Domains\\{$domain}\\Http\\{$this->folder}\\".$this->argument('name'));
+        }
+
+        return true;
+    }
+
+    protected function getDomains(): array
+    {
+        // Retrieve the list of domains from the Domains folder
+        $domainsDir = app_path('Domains');
+        $domains = array_diff(scandir($domainsDir), ['.', '..']);
+
+        return array_values($domains);
+    }
+
+    protected function getDomainOption(): array
+    {
+        return ['domain', 'd', InputOption::VALUE_NONE, 'Prompt for the domain to create the class in'];
+    }
+}
